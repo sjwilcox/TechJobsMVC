f62182db3c95bebc614929c27f6ca87b5b535862
steven.j.wilcox@gmail.com <steven.j.wilcox@gmail.com>
2021-07-26 19:30:56 -0400

    basic complete

--- TechJobsMVC/Controllers/SearchController.cs
+++ TechJobsMVC/Controllers/SearchController.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using TechJobsMVC.Data;
+using TechJobsMVC.Models;
 
 // For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
 
@@ -11,6 +13,7 @@ namespace TechJobsMVC.Controllers
     public class SearchController : Controller
     {
         // GET: /<controller>/
+        
         public IActionResult Index()
         {
             ViewBag.columns = ListController.ColumnChoices;
@@ -18,5 +21,24 @@ namespace TechJobsMVC.Controllers
         }
 
         // TODO #3: Create an action method to process a search request and render the updated search view. 
+        
+        public IActionResult Results(string searchType, string searchTerm)
+        {
+            ViewBag.columns = ListController.ColumnChoices;
+            List<Job> jobs;
+            if(string.IsNullOrEmpty(searchTerm))
+            {
+                jobs = JobData.FindAll();
+            }
+            else
+            {
+                jobs = JobData.FindByColumnAndValue(searchType, searchTerm);
+            }
+
+            ViewBag.jobSearch = jobs;
+            return View("Index");
+            
+            
+        }
     }
 }
